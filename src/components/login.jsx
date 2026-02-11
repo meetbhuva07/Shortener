29fa6c4e8322c59f04db5c48e0014e325147ff82
meetbhuva07 <meetbhuva0009@gmail.com>
2026-02-11 09:12:16 +0530

    login error parts are completed

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Card,
   CardAction,
@@ -12,8 +12,44 @@ import { Input } from './ui/input'
 import { Button } from './ui/button'
 import { BeatLoader } from 'react-spinners'
 import Error from './error'
+import * as Yup from 'yup'
 
 const Login = () => {
+const [errors, setErrors] = useState([])
+const [fromData, setFromData] = useState({
+  email:"",
+  password:""
+})
+
+const handleInputChange = (e) => {
+  const {name,value} = e.target
+  setFromData((prevState) => ({
+    ...prevState,
+    [name]: value,
+  }))
+}
+
+const handleLogin = async () => {
+setErrors([])
+try {
+  const schema = Yup.object().shape({
+    email:Yup.string().email("Invalid Email").required("Email is Required"),
+    password:Yup.string().min(6,"password must be at least 6 characters").required("password is Required")
+  })
+
+  await schema.validate(fromData,{abortEarly:false})
+    // api call
+} catch (e) {
+  const newErrors = {}
+
+  e?.inner?.forEach((err) => {
+     newErrors[err.path] = err.message;    
+  });
+
+  setErrors(newErrors)
+}
+}
+ 
   return (
     <Card>
   <CardHeader>
@@ -23,16 +59,16 @@ const Login = () => {
   </CardHeader>
   <CardContent className='space-y-2'>
     <div className="space-y-1">
-        <Input name = 'email' type='email' placeholder = "Enter Email" />
-        <Error message={"Some error"} />
+        <Input name = 'email' type='email' placeholder = "Enter Email" onChange={handleInputChange}/>
+        {errors.email && <Error message={errors.email} /> }
     </div>
     <div className="space-y-1">
-        <Input name = 'password' type='password' placeholder = "Enter Password" />
-        <Error message={"Some error"} />
+        <Input name = 'password' type='password' placeholder = "Enter password"  onChange={handleInputChange} />
+        {errors.password && <Error message={errors.password} /> }
     </div>
   </CardContent>
   <CardFooter>
-    <Button>
+    <Button  onClick={handleLogin}>
         {true?<BeatLoader size={10} color='#111827'/>:"Login"}
     </Button>
   </CardFooter>
